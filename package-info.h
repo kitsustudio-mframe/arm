3317b9a91037a2f7ebf45f429e3baa2fc7c95d3d
ZxyKira <danny1191365@kitsu-studio.net>
2023-06-11 23:14:26 +0800

    [fix] add using define

--- package-info.h
+++ package-info.h
@@ -14,28 +14,93 @@
 /* ****************************************************************************************
  * Include
  */
+#ifdef USING_ARM_COMMON_TABLES
 #include "arm_common_tables.h"
+#endif
+
+#ifdef USING_ARM_CONST_STRUCTS
 #include "arm_const_structs.h"
+#endif
+
+#ifdef USING_ARM_HELIUM_UTILS
 #include "arm_helium_utils.h"
+#endif
+
+#ifdef USING_ARM_MATH
 #include "arm_math.h"
+#endif
+
+#ifdef USING_ARM_MVE_TABLES
 #include "arm_mve_tables.h"
+#endif
+
+#ifdef USING_ARM_VEC_MATH
 #include "arm_vec_math.h"
+#endif
+
+#ifdef USING_CMSIS_ARMCC
 #include "cmsis_armcc.h"
+#endif
+
+#ifdef USING_CMSIS_ARMCC_V6
 #include "cmsis_armcc_V6.h"
+#endif
+
+#ifdef USING_CMSIS_ARMCLANG
 #include "cmsis_armclang.h"
+#endif
+
+#ifdef USING_CMSIS_ARMCLANG_LTM
 #include "cmsis_armclang_ltm.h"
+#endif
+
+#ifdef USING_CMSIS_COMPILER
 #include "cmsis_compiler.h"
+#endif
+
+#ifdef USING_CMSIS_GCC
 #include "cmsis_gcc.h"
+#endif
+
+#ifdef USING_CMSIS_IAR
 #include "cmsis_iar.h"
+#endif
+
+#ifdef USING_CMSIS_ICCARM
 #include "cmsis_iccarm.h"
+#endif
+
+#ifdef USING_CMSIS_VERSION
 #include "cmsis_version.h"
+#endif
+
+#ifdef USING_CORE_CM4
 #include "core_cm4.h"
+#endif
+
+#ifdef USING_CORE_CMFUNC
 #include "core_cmFunc.h"
+#endif
+
+#ifdef USING_CORE_CMINSTR
 #include "core_cmInstr.h"
+#endif
+
+#ifdef USING_CORE_CMSIMD
 #include "core_cmSimd.h"
+#endif
+
+#ifdef USING_MPU_ARMV7
 #include "mpu_armv7.h"
+#endif
+
+#ifdef USING_MPU_ARMV8
 #include "mpu_armv8.h"
+#endif
+
+#ifdef USING_PMU_ARMV8
 #include "pmu_armv8.h"
+#endif
 
 /* ****************************************************************************************
  * End of file
