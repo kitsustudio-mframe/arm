42e492d66c9f9976e3aad375d37f62c9b9d5bb57
ZxyKira <danny1191365@kitsu-studio.net>
2023-08-27 22:40:09 +0800

    add include using macro

--- package-info.h
+++ package-info.h
@@ -16,90 +16,112 @@
  */
 #ifdef USING_ARM_COMMON_TABLES
 #include "arm_common_tables.h"
+#undef USING_ARM_COMMON_TABLES
 #endif
 
 #ifdef USING_ARM_CONST_STRUCTS
 #include "arm_const_structs.h"
+#undef USING_ARM_CONST_STRUCTS
 #endif
 
 #ifdef USING_ARM_HELIUM_UTILS
 #include "arm_helium_utils.h"
+#undef USING_ARM_HELIUM_UTILS
 #endif
 
 #ifdef USING_ARM_MATH
 #include "arm_math.h"
+#undef USING_ARM_MATH
 #endif
 
 #ifdef USING_ARM_MVE_TABLES
 #include "arm_mve_tables.h"
+#undef USING_ARM_MVE_TABLES
 #endif
 
 #ifdef USING_ARM_VEC_MATH
 #include "arm_vec_math.h"
+#undef USING_ARM_VEC_MATH
 #endif
 
 #ifdef USING_CMSIS_ARMCC
 #include "cmsis_armcc.h"
+#undef USING_CMSIS_ARMCC
 #endif
 
 #ifdef USING_CMSIS_ARMCC_V6
 #include "cmsis_armcc_V6.h"
+#undef USING_CMSIS_ARMCC_V6
 #endif
 
 #ifdef USING_CMSIS_ARMCLANG
 #include "cmsis_armclang.h"
+#undef USING_CMSIS_ARMCLANG
 #endif
 
 #ifdef USING_CMSIS_ARMCLANG_LTM
 #include "cmsis_armclang_ltm.h"
+#undef USING_CMSIS_ARMCLANG_LTM
 #endif
 
 #ifdef USING_CMSIS_COMPILER
 #include "cmsis_compiler.h"
+#undef USING_CMSIS_COMPILER
 #endif
 
 #ifdef USING_CMSIS_GCC
 #include "cmsis_gcc.h"
+#undef USING_CMSIS_GCC
 #endif
 
-#ifdef USING_CMSIS_IAR
+#ifdef  USING_CMSIS_IAR
 #include "cmsis_iar.h"
+#undef USING_CMSIS_IAR
 #endif
 
 #ifdef USING_CMSIS_ICCARM
 #include "cmsis_iccarm.h"
+#undef USING_CMSIS_ICCARM
 #endif
 
 #ifdef USING_CMSIS_VERSION
 #include "cmsis_version.h"
+#undef USING_CMSIS_VERSION
 #endif
 
 #ifdef USING_CORE_CM4
 #include "core_cm4.h"
+#undef USING_CORE_CM4
 #endif
 
 #ifdef USING_CORE_CMFUNC
 #include "core_cmFunc.h"
+#undef USING_CORE_CMFUNC
 #endif
 
 #ifdef USING_CORE_CMINSTR
 #include "core_cmInstr.h"
+#undef USING_CORE_CMINSTR
 #endif
 
 #ifdef USING_CORE_CMSIMD
 #include "core_cmSimd.h"
+#undef USING_CORE_CMSIMD
 #endif
 
 #ifdef USING_MPU_ARMV7
 #include "mpu_armv7.h"
+#undef USING_MPU_ARMV7
 #endif
 
 #ifdef USING_MPU_ARMV8
 #include "mpu_armv8.h"
+#undef USING_MPU_ARMV8
 #endif
 
 #ifdef USING_PMU_ARMV8
 #include "pmu_armv8.h"
+#undef USING_PMU_ARMV8
 #endif
 
 /* ****************************************************************************************
